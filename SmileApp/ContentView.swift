aaa59f4aa1e3d7ae13482da08dc3ec19de18b804
minesyouto <minesyouto8833@gmail.com>
2022-01-30 22:04:19 +0900

    Initial Commit

---
+++ SmileApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmileApp
+//
+//  Created by みねた on 2022/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmileApp/SmileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileAppApp.swift
+//  SmileApp
+//
+//  Created by みねた on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SmileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
